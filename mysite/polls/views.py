124c8fafcff185187158a81848bf8cf6994aa275
yangchi <yang.chi@utooo.com>
2018-12-13 17:52:28 +0800

    更改

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Question
+from .models import Question,Choice
 # Create your views here.
 
 
