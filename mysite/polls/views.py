7f964b903b1123cadd3bbd26aa76b5d100cc0f5c
yangchi <18871430038@163.com>
2018-12-16 20:26:08 +0800

    上传

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.utils import timezone
 # Create your views here.
 
 
@@ -11,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
